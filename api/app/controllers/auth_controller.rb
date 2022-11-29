71359926ef58c7c1467122d4ff184b94b6cc4fcf
Avoarisera <jenrandr@gmail.com>
2022-11-29 11:22:16 +0300

    addd sere

--- api/app/controllers/auth_controller.rb
+++ api/app/controllers/auth_controller.rb
@@ -1,17 +1,28 @@
 class AuthController < ApplicationController
+  before_action :set_user
   # POST /sign_in
   def sign_in
     pp "***************"
     pp params
-    @user = User.find_by_email(params[:email])
-    pp @user
-    if @user.authenticate(params[:password]) == false
+    pp set_user
+    if set_user.authenticate(params[:password]) == false
       raise "invalid credentials"
     else
     # sets up token
+    pp "token"
+    pp token
     token = generate_token({ uid: @user.id })
 
     render json: { auth: token }, status: :created
+    end
+  end
+
+  private
+  def set_user
+    begin
+      @user = User.find_by_email(params[:email])
+    rescue ActiveRecord::RecordNotFound
+      return
     end
   end
 end
